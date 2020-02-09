7ea11478a73c1103057aafe9f83923b2fc40566e
Sarbajit Nandy <sarbajitnandey@gmail.com>
2020-02-09 15:26:58 +0530

    Fali-safe added successfully

--- Scripts/GUI/addSheetDetails.py
+++ Scripts/GUI/addSheetDetails.py
@@ -43,9 +43,11 @@ class Ui_MainWindow(object):
         font.setPointSize(12)
         self.file2SheetName.setFont(font)
         self.file2SheetName.setObjectName("file2SheetName")
-        self.btn = QtWidgets.QPushButton(self.centralwidget)
-        self.btn.setGeometry(QtCore.QRect(360, 250, 75, 23))
-        self.btn.setObjectName("btn")
+        self.statusView = QtWidgets.QTextBrowser(self.centralwidget)
+        self.statusView.setEnabled(True)
+        self.statusView.setGeometry(QtCore.QRect(20, 230, 431, 111))
+        self.statusView.setReadOnly(True)
+        self.statusView.setObjectName("statusView")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 618, 21))
@@ -63,7 +65,14 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.lable_sheet1.setText(_translate("MainWindow", "TextLabel"))
         self.lable_sheet2.setText(_translate("MainWindow", "TextLabel"))
-        self.btn.setText(_translate("MainWindow", "PushButton"))
+        self.statusView.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><title>JS Bin</title><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:12pt; font-weight:400; font-style:normal;\">\n"
+"<h3 style=\" margin-top:14px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:large; font-weight:600;\">Sarbajit</span> </h3>\n"
+"<ol style=\"margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; -qt-list-indent: 1;\"><li style=\" margin-top:12px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">123 </li>\n"
+"<li style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">345 </li>\n"
+"<li style=\" margin-top:0px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">567 </li></ol></body></html>"))
 
 
 if __name__ == "__main__":
